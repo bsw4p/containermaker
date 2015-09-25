5b7917fb11ab9d5284fa865990eef1a6f200535b
Benedikt Schmotzle <code@schmotzle.info>
2015-09-25 15:51:48 +0200

    Adding config.mk

---
+++ config.mk
@@ -0,0 +1,65 @@
+#-----------------------------------------------------------------
+# Namespace where this container shoud be stored in
+# [a-z0-9_] are allowed, size between 4 and 30 for example:
+#-----------------------------------------------------------------
+#container_namespace=
+
+#-----------------------------------------------------------------
+# Name of this container
+# [a-z0-9_] are allowed, size between 4 and 30 for example: sshd
+#-----------------------------------------------------------------
+container_name=IdontKnowWhatIAmDoing
+
+#-----------------------------------------------------------------
+# Uncomment if docker programm should be called with sudo
+#-----------------------------------------------------------------
+#use_sudo     = YES
+
+#-----------------------------------------------------------------
+# Uncomment if application is running inside a terminal
+#-----------------------------------------------------------------
+#terminal     = YES
+
+
+#-----------------------------------------------------------------
+# Set if a port from inside the docker container should be mapped
+# to the host system
+#-----------------------------------------------------------------
+#port_intern  =
+
+
+#-----------------------------------------------------------------
+# Set if a port from inside the docker container should be mapped
+# to the host system using a different port than the inside port
+# defined by port_intern
+#-----------------------------------------------------------------
+#port_extern  =
+
+
+#-----------------------------------------------------------------
+# Set if a directory from the host machine should be mounted
+# to inside the container.
+# Use a path relative to the directory this makefile is saved in.
+#-----------------------------------------------------------------
+#in_mount_from=
+
+#-----------------------------------------------------------------
+# Set if a directory from the host machine should be mounted
+# to inside the container.
+# Use a absolute path inside the container.
+#-----------------------------------------------------------------
+#in_mount_to=
+
+
+#-----------------------------------------------------------------
+# Set if a directory from the host machine should be mounted
+# to inside the container as a output folder (read-write).
+# Use a path relative to the directory this makefile is saved in.
+#-----------------------------------------------------------------
+#out_mount_from=
+#-----------------------------------------------------------------
+# Set if a directory from the host machine should be mounted
+# to inside the container.
+# Use a absolute path inside the container.
+#-----------------------------------------------------------------
+#out_mount_to=
